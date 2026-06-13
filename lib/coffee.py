d78c673c2adf3933447fa75b4f501e53163a3de2
Lascelles <waunaneville@gmail.com>
2026-06-13 07:51:33 +0300

    feat: add Book class with encapsulated properties and page-turning behavior - Implemented title property with getter and setter - Implemented page_count property with validation to ensure integer values - Added turn_page method to simulate flipping through the book - Enforced encapsulation by using private attributes _title and _page_count

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self._size = size
+        self._price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        valid_sizes = ("Small", "Medium", "Large")
+        if value not in valid_sizes:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        self._price = value
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self._price += 1.0
+
+
+
